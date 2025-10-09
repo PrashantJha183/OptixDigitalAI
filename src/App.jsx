ee58bf0ab9535baf3ff4627b00135c1aa56247f8
Prashant Jha <jhap5754@gmail.com>
2025-10-09 16:59:27 +0530

    Configured tailwind css correctly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 function App() {
   return (
-    
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-900 p-4">
       {/* Main Heading */}
       <h1
